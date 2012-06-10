7db347f732332e2df234e7789707f31c6d07a140
Dean Sofer <proloser@hotmail.com>
2012-06-10 02:16:17 -0700

    Removed the `generateSig()` function since it is not used by Vimeo
    Also removed other vestigial changes that were only used by Flickr

--- Model/Datasource/Vimeo.php
+++ Model/Datasource/Vimeo.php
@@ -28,26 +28,11 @@ class Vimeo extends ApisSource {
     );
 
     public function beforeRequest(&$model, $request) {
-        $request['uri']['query']['api_key'] = $this->config['login'];
         if (!empty($request['uri']['path'])) {
             $request['uri']['query']['method'] = $request['uri']['path'];
             $request['uri']['path'] = '';
         }
         $request['uri']['query']['format'] = $this->options['format'];
-        $request['uri']['query']['nojsoncallback'] = 1;
-        if (!empty($this->config['oauth_token']))
-            $request['uri']['query']['auth_token'] = $this->config['oauth_token'];
-        $request['uri']['query']['api_sig'] = $this->generateSig($request['uri']['query']);
         return $request;
-    }
-
-    public function generateSig($params) {
-        $sig = $this->config['password'];
-        ksort($params);
-        foreach ($params as $key => $value) {
-            $sig .= $key . $value;
-        }
-        $sig = md5($sig);
-        return $sig;
     }
 }
